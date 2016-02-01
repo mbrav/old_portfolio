d002eb61e0f09f86ec9a8e289e12bc0b416b9550
mixania <mbraverman96@gmail.com>
2016-02-01 14:49:29 -0500

    Whats GUCCI?

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,14 +4,25 @@ var x2 = 255;
 
 function setup() {
   createCanvas(600,600);
-  background(255,255,0);
+  background(191,183,159);
+  noStroke();
 }
 
 function draw() {
   x ++;
   x2 --;
-  background(x2,x,0);
-  rect(0,0,mouseX, mouseY);
+  background(191,183,159);
+  fill(79,107,98);
+  rect(0,0, mouseX, mouseY);
+  textSize(40);
+
+  if (x % 10 < 5) {
+    fill(255,255,255);
+  } else {
+    fill(143,16,16);
+  }
+  text("Whats", 200, 300);
+  text("GUCCI?", 200, 340);
   console.log(x);
   console.log(x2);
 }
