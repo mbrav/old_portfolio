edbd5d7e4ba970c0b94865b783cc34b64f037e40
mixania <mbraverman96@gmail.com>
2016-02-03 16:59:25 -0500

    Added coll rectangle transitions

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,23 +1,44 @@
-var x = 0;
-var x2 = 255;
+var x = 100;
+var y = 100;
 
 var xDiff;
 var yDiff;
+
+var randomX;
+var randomY;
 
 function setup() {
   createCanvas(600,600);
   background(191,183,159);
   noStroke();
+  generateCordinate();
 }
 
 function draw() {
-  x ++;
-  x2 --;
+
+  if (randomX == x || randomY == y) {
+    // if the cordinates reach the number
+    // generate a new one
+     generateCordinate();
+     console.log("GENERATE!");
+  } else {
+    if (randomX > x) {
+      x ++;
+    } else {
+      x --;
+    }
+    if (randomY > y) {
+      y ++;
+    } else {
+      y --;
+    }
+  }
+
   background(191,183,159);
   fill(79,107,98);
 
-  xDiff = mouseX - width/2;
-  yDiff = mouseY - height/2;
+  xDiff = x - width/2;
+  yDiff = y - height/2;
 
   rect(width/2 + xDiff, height/2 + yDiff, -xDiff, yDiff);
   rect(width/2 + xDiff, height/2 - yDiff, xDiff, yDiff);
@@ -31,8 +52,14 @@ function draw() {
   } else {
     fill(143,16,16);
   }
+
   text("Whats", 200, 300);
   text("GUCCI?", 200, 340);
-  console.log(x);
-  console.log(x2);
+  console.log(randomX);
+  console.log(randomY);
+}
+
+function generateCordinate() {
+  randomX = int(random(0,width));
+  randomY = int(random(0,height));
 }
