e3dc98540534a0f9ecf5b98a56a31c7b3db69023
mixania <mbraverman96@gmail.com>
2016-02-12 21:15:17 -0500

    Oulipo: working poem parser

--- projects/oulipo/script.js
+++ projects/oulipo/script.js
@@ -1,1 +1,25 @@
 // Oulipo
+
+var primeNumbers = [2,3,5,7,11,13,17,19];
+
+function generate(id) {
+    var textBox = document.getElementById(id);
+
+    // get the text from the text field
+    var rawText = textBox.value;
+
+    // split the text into a word array
+    var arrayText = rawText.split(" ");
+    var poemArray = [];
+    var add = 0;
+
+    // generate the poem into the poemArray
+    for (var i = 0; i < arrayText.length * 10; i++) {
+
+        add += primeNumbers[i % primeNumbers.length];
+        add = (add + i) % arrayText.length;
+
+        console.log(arrayText[add]);
+        poemArray.push = arrayText[add];
+    }
+}
