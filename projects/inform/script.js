24fdfc48da344605262933cd0beadb44af39595d
mixania <mbraverman96@gmail.com>
2016-03-13 13:21:01 -0400

    in.fORM: made div elements as seperate views

--- projects/inform/script.js
+++ projects/inform/script.js
@@ -1,27 +1,51 @@
 //infORM alpha
 //created by Michael Braverman on March 12, 2016
 
-var title, aboutText;
+// DOM elements
+var title, aboutText, textField, submitButton, backButton;
 
+// App elements
+var rawInputText;
 
 function setup() {
   noCanvas();
 
   title = select('#title');
   aboutText = select('#about-text');
+  textField = select('#text-field');
+  submitButton = select('#submit-button');
+  backButton = select('#back-button');
   aboutText.hide();
 
-  title.mouseOver(function showt(){
+  submitButton.mousePressed(submit);
+  backButton.mousePressed(back);
+
+  title.mouseOver(function show(){
     aboutText.show();
     console.log("about-text: show");
   });
 
-  title.mouseOut(function hidet(){
+  title.mouseOut(function hide(){
     aboutText.hide();
     console.log("about-text: hide");
   });
-
 }
 
 function draw() {
+}
+
+function submit() {
+  rawInputText = textField.value();
+  console.log(rawInputText);
+  var submitBody = select('#submit-page');
+  var mapBody = select('#map-page');
+  submitBody.hide();
+  mapBody.show();
+}
+
+function back() {
+  var submitBody = select('#submit-page');
+  var mapBody = select('#map-page');
+  submitBody.show();
+  mapBody.hide();
 }
