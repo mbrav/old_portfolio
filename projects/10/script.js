42b834a029fa67f7e0af2a7961b6d3a6887e8575
mixania <mbraverman96@gmail.com>
2016-04-17 15:41:54 -0400

    added cool canvas animation

--- projects/10/script.js
+++ projects/10/script.js
@@ -2,37 +2,57 @@
 //created by Michael Braverman on April 16, 2016
 
 
-// animation canvas
-var p5Animation = new p5(function(sketch) {
-  var x = 100, y = 100;
+var animation = function( p ) {
+  // attributes
+  var w = 250;
+  var h = 250;
 
-  sketch.setup = function() {
-    var anim = sketch.createCanvas(150, 150);
-    anim.position(300, 50);
-    anim.background(20);
-    anim.class('animation');
+  p.setup = function() {
+    p.createCanvas(w, h, WEBGL).position(windowWidth/2 - w/2, 150);
   };
 
-  sketch.draw = function() {
-    anim.stroke(200);
-    anim.line(10, 10, 50, 50);
+  p.draw = function() {
+
+    p.background(0);
+    p.fill(0);
+    p.normalMaterial();
+
+    // "wtf? this is so cool!"
+    for (var x = 0; x < 4; x++) {
+      for (var y = 0; y < 4; y++) {
+        p.translate(0, 0, 0);
+        p.translate(20 * x, 20 * y, 0);
+        p.push();
+        p.box(10, 10, 0);
+        p.pop();
+        p.rotateZ(frameCount * 0.02);
+        p.rotateX(frameCount * 0.01);
+        p.rotateY(frameCount * 0.01);
+
+      }
+    }
   };
-});
+};
+
+// apply animation sketch
+var animationSketch = new p5(animation);
+var body;
 
 
+// p5.js noCanvas DOM stuff
 function setup() {
   // colorMode(HSB);
-  createCanvas(150, 150);
-  // noCanvas();
+  noCanvas();
   background(20);
+
 
   words = ["hello", "nuts", "dinoman", "dineomite", "blow", "bite", "zebra", "hydra", "fuck", "shit"];
 
   var button1 = select('#button-one');
   var button2 = select('#button-two');
 
-  button1.position(windowWidth/3, windowHeight - 200);
-  button2.position(windowWidth - windowWidth/3, windowHeight - 200);
+  button1.position(windowWidth/3 - 25, windowHeight - 200);
+  button2.position(windowWidth - windowWidth/3 - 25, windowHeight - 200);
 
   button1.mouseClicked(one);
   button2.mouseClicked(two);
