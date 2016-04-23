2fdbfa82b15964751450f8e17db0820f70386019
mixania <mbraverman96@gmail.com>
2016-04-23 17:44:28 -0400

    Revoked key

--- projects/11/config.js
+++ projects/11/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         "OrrYFfgYRacMIoDjAkA2JrS6L",
-  consumer_secret:      "p0mchdPZN5FUldXum7ATQLtijenGqcrJ7NLD8I7uHEHouveCLk",
-  access_token:         "237022647-Ob65nrbloua44LPo881DXRaY7QDsmu1b82PKwU9t",
-  access_token_secret:  "T4JSFdt2dpnPu9bMf7tFOfpTTrwLDbDIorfDEk8e2SG8m",
+  consumer_key:         "",
+  consumer_secret:      "",
+  access_token:         "",
+  access_token_secret:  "",
   timeout_ms:           60*1000
 }
