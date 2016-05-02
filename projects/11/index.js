aa0bf8fbc1238942242a1d18593a32cc1c0f8812
mixania <mbraverman96@gmail.com>
2016-05-01 20:26:57 -0400

    Serve html file

--- projects/11/index.js
+++ projects/11/index.js
@@ -2,15 +2,24 @@
 //created by Michael Braverman on April 18, 2016
 
 var http = require('http');
+var fs = require('fs'); // filw sysytem
 var module1 = require('./module1');
 
 http.createServer(onRequest).listen(8000);
 
 function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(module1.myString);
-  module1.myFunction();
-  response.end(); // end response
+  // response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'html'});
+
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found!');
+    }  else {
+      response.write(data);
+    }
+    response.end();
+  });
 }
 
 // var Twit = require('twit');
