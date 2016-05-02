8efdcf07013bad028308296d4b6e1f53bc29d3f8
mixania <mbraverman96@gmail.com>
2016-05-02 14:15:11 -0400

    Rita completed

--- projects/09/script.js
+++ projects/09/script.js
@@ -12,7 +12,21 @@ function generate() {
   var rawText = select('.enter').value();
   var words = RiTa.tokenize(rawText);
 
+  var pluralizedWords = [];
+  var participleWords = [];
+  var phonemes = [];
+  var syllables = [];
   for (var i = 0; i < words.length; i++) {
-    createP(words[i]).position(random(windowWidth), random(windowHeight));
+    pluralizedWords[i] =  RiTa.pluralize(words[i]);
+    participleWords[i] = RiTa.getPastParticiple(words[i]);
+    phonemes[i] = RiTa.getPhonemes(words[i]);
+    syllables[i] = RiTa.getSyllables(words[i]);
+  }
+  for (var i = 0; i < words.length; i++) {
+    createP(words[i]).position(random(windowWidth), random(windowHeight)).class('strikeout');
+    createP(pluralizedWords[i]).position(random(windowWidth), random(windowHeight)).class('strikeout');
+    createP(participleWords[i]).position(random(windowWidth), random(windowHeight)).class('strikeout');
+    createP(phonemes[i]).position(random(windowWidth), random(windowHeight)).class('strikeout');
+    createP(syllables[i]).position(random(windowWidth), random(windowHeight)).class('strikeout');
   }
 }
