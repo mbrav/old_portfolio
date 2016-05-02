effb9d8579e164554a5623663c3668c6f3e51a8b
mixania <mbraverman96@gmail.com>
2016-05-02 13:52:52 -0400

    Rita: fixed crash

--- projects/09/script.js
+++ projects/09/script.js
@@ -1,23 +1,18 @@
 function setup() {
   noCanvas();
 
-  var words = RiTa.tokenize("The elephant took a bite!")
-
-  console.log(words);
-  console.log(RiTa.getPosTags("I am hungry, The elephant took a bite"));
-
-  var rm = new RiMarkov(4);
-  var rm = new RiMarkov(4);
-  rm.loadText("The girl went to a game after dinner. The teacher went to dinner with a girl.");
-  var sentences = rm.generateSentences(8);
-
-  console.log(sentences);
-
-  for (var i = 0; i < 8; i++) {
-    createP(sentences);
-  }
 }
 
 function draw() {
 
+}
+
+function generate() {
+  // get the element from the text field
+  var rawText = select('.enter').value();
+  var words = RiTa.tokenize(rawText);
+
+  for (var i = 0; i < words.length; i++) {
+    createP(words[i]).position(random(windowWidth), random(windowHeight));
+  }
 }
