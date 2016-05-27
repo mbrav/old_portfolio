1803a4995d725e984ee307d378a2f91d99091b51
mixania <mbrav@protonmail.com>
2016-05-27 09:23:00 +0200

    CSS for Mobile

--- portfolio/src/script.js
+++ portfolio/src/script.js
@@ -4,36 +4,38 @@
 var pageDuration = 700;
 
 
-// TODO - chnage the way current pages are interacted with
- $("#about-link").click(function() {
-   $("li").removeClass("selected-page");
-   $(this).addClass("selected-page");
-   $("main").hide(pageDuration).css("display", "none");
-   $("#about")
-   .fadeIn(pageDuration)
-   .css("display", "inline-block");
- });
- $("#projects-link").click(function() {
-   $("li").removeClass("selected-page");
-   $(this).addClass("selected-page");
-   $("main").hide(pageDuration).css("display", "none");
-   $("#projects")
-   .fadeIn(pageDuration)
-   .css("display", "inline-block");
- });
- $("#resume-link").click(function() {
-   $("li").removeClass("selected-page");
-   $(this).addClass("selected-page");
-   $("main").hide(pageDuration).css("display", "none");
-   $("#resume")
-   .fadeIn(pageDuration)
-   .css("display", "inline-block");
- });
- $("#contact-link").click(function() {
-   $("li").removeClass("selected-page");
-   $(this).addClass("selected-page");
-   $("main").hide(pageDuration).css("display", "none");
-   $("#contact")
-   .fadeIn(pageDuration)
-   .css("display", "inline-block") ;
- });
+$(function() {
+  // TODO - chnage the way current pages are interacted with
+  $("#about-link").click(function() {
+    $("li").removeClass("selected-page");
+    $(this).addClass("selected-page");
+    $("main").hide(pageDuration).css("display", "none");
+    $("#about")
+    .fadeIn(pageDuration)
+    .css("display", "inline-block");
+  });
+  $("#projects-link").click(function() {
+    $("li").removeClass("selected-page");
+    $(this).addClass("selected-page");
+    $("main").hide(pageDuration).css("display", "none");
+    $("#projects")
+    .fadeIn(pageDuration)
+    .css("display", "inline-block");
+  });
+  $("#resume-link").click(function() {
+    $("li").removeClass("selected-page");
+    $(this).addClass("selected-page");
+    $("main").hide(pageDuration).css("display", "none");
+    $("#resume")
+    .fadeIn(pageDuration)
+    .css("display", "inline-block");
+  });
+  $("#contact-link").click(function() {
+    $("li").removeClass("selected-page");
+    $(this).addClass("selected-page");
+    $("main").hide(pageDuration).css("display", "none");
+    $("#contact")
+    .fadeIn(pageDuration)
+    .css("display", "inline-block") ;
+  });
+});
