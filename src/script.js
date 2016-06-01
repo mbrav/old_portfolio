bc1fb300c8e94313e523c93cc396f7e3dd86139a
mixania <mbrav@protonmail.com>
2016-06-01 20:34:54 +0200

    Imprived Image descriptions

--- src/script.js
+++ src/script.js
@@ -1,25 +1,47 @@
 //Personal Site
 //created by Michael Braverman on April 16, 2016
 
-var images = ['01.jpg', '02.png', '03.png', '04.jpg', '05.png',
-            '06.png','07.png','08.jpg'];
+var imgData = [
+    {
+      'file':'01.jpg',
+      'name':'Artificial Intellect Box',
+      'year': 2015},
+    {
+      'file':'02.png',
+      'name':'Infrastructural Utopia',
+      'year': 2016},
+    {
+      'file':'03.png',
+      'name':'infORM alpha',
+      'year': 2016},
+    {
+      'file':'04.jpg',
+      'name':'Poem Maschine',
+      'year': 2015},
+    {
+      'file':'05.png',
+      'name':'Emotion Cube',
+      'year': 2015},
+    {
+      'file':'06.png',
+      'name':'Multiverse Clockwork',
+      'year': 2016},
+    {
+      'file':'07.png',
+      'name':'Flowing Pagoda',
+      'year': 2015},
+    {
+      'file':'08.jpg',
+      'name':'The Endevours Guide to The 21st Century',
+      'year': 2015}
+];
 
-
-var text = ['Artificial Intellect Box (2015)',
-            'Infrastructural Utopia (2016)',
-            'infORM alpha (2016)',
-            'Poem Maschine (2015)',
-            'Emotion Cube (2015)',
-            'Multiverse Clockwork (2016)',
-            'Flowing Pagoda (2015)',
-            'The Endevours Guide to The 21st Century (2015)'
-            ];
 var imageIndex = Math.floor(Math.random() * images.length);
 
 $(".crop-img > img").click(function() {
   imageIndex = (imageIndex + 1) % (images.length);
-  $(this).attr('src', "src/img/" + images[imageIndex]);
-  $(".img-caption").text(text[imageIndex]);
+  $(this).attr('src', "src/img/" + imgData[imageIndex]["file"]);
+  $(".img-caption").text(imgData[imageIndex]["name"]);
 })
 
 var pageDuration = 700;
