d99926c42c9eddeeaa64f3b806027905f3c55db4
mixania <mbrav@protonmail.com>
2016-06-07 13:31:23 +0200

    Working image tiles, testing animations
    * added animations.css

--- src/script.js
+++ src/script.js
@@ -9,6 +9,12 @@ var projectData = [
       'year': 2015,
     },
     {
+      'imgFile':'01.jpg',
+      'page':'intellect.html',
+      'name':'Artificial Intellect Box',
+      'year': 2015,
+    },
+    {
       'imgFile':'02.png',
       'page':'utopia-tower.html',
       'name':'Infrastructural Utopia Tower',
@@ -30,18 +36,19 @@ var projectData = [
       'year': 2015
     },
     {
-      'imgFile':'08.jpg',
-      'name':'The Endevours Guide to The 21st Century',
-      'year': 2015
-    },
-    {
       'imgFile':'06.png',
       'name':'Multiverse Clockwork',
+      'page':'multiverse-clockwork.html',
       'year': 2016
     },
     {
       'imgFile':'07.png',
       'name':'Flowing Pagoda',
+      'year': 2015
+    },
+    {
+      'imgFile':'08.jpg',
+      'name':'The Endevours Guide to The 21st Century',
       'year': 2015
     }
 ];
@@ -54,7 +61,7 @@ $(function() {
   var pageDuration = 700;
 
   // change image slide on click
-  $(".slide-img").click(function() {
+  $("#slide-img").click(function() {
     changeImg();
   })
 
@@ -70,29 +77,32 @@ $(function() {
         } else {
           return "grid-item";
         }
-      }).addClass(function(){
-        // make a wide tile every other 3 tiles
-        if ( i % 3 == 0 && i !=0) {
-          return "grid-item--width2";
-        } else {
-          return null;
-        }
-      }).addClass(function(){
-        // make a tall tile every other 3 tiles
-        if ( (i+1) % 3 == 0 && i !=0) {
+      })
+      // make a tall tile every other 3 tiles
+      .addClass(function(){
+        if ( i % 2 == 0 && i !=0) {
           return "grid-item--height2";
         } else {
           return null;
         }
       })
+      // project image in the background
+      .css({
+        "background-image" : "url(src/img/" + projectData[i]["imgFile"] + ")",
+        "background-size" : "cover",
+        "background-position" : "center"
+      })
+      // content that goes inside each grid element
       .append(
-        // link to project
-        $("<a>", {
-          href : "/pages/" + projectData[i]["page"],
-          text : projectData[i]["name"]
-        }),
-        // span same level as <a>
-        $("<span>", {text : projectData[i]["year"]})
+        // $("<div>").addClass("grid-description").append(
+          // link to project
+          $("<a>", {
+            href : "/pages/" + projectData[i]["page"],
+            text : projectData[i]["name"]
+          }),
+          // span same level as <a>
+          $("<span>", {text : projectData[i]["year"]})
+        // )
       )
     );
   }
@@ -119,8 +129,8 @@ $(function() {
     $("nav ul").removeClass("show-nav-mobile");
   });
   $("#projects-link").click(function() {
-    $("li").removeClass("selected-page");
-    $(this).addClass("selected-page");
+    $("li").removeClass("selected-link");
+    $(this).addClass("selected-link");
     $("main").fadeOut(pageDuration).css("display", "none");
     $("#projects").fadeIn(pageDuration);
 
@@ -128,15 +138,18 @@ $(function() {
     $("nav ul").removeClass("show-nav-mobile");
 
     // layout tiles using Masonry jQuery plugin
-    $("#project-grid").masonry({
+    var projectGrid = $("#project-grid").masonry({
       itemSelector: '.grid-item',
       columnWidth: '.grid-sizer',
       percentagePosition: true
     });
+
+    // load masonry
+    projectGrid.masonry('reloadItems');
   });
   $("#gallery-link").click(function() {
-    $("li").removeClass("selected-page");
-    $(this).addClass("selected-page");
+    $("li").removeClass("selected-link");
+    $(this).addClass("selected-link");
     $("main").fadeOut(pageDuration).css("display", "none");
     $("#gallery").fadeIn(pageDuration);
 
@@ -144,8 +157,8 @@ $(function() {
     $("nav ul").removeClass("show-nav-mobile");
   });
   $("#resume-link").click(function() {
-    $("li").removeClass("selected-page");
-    $(this).addClass("selected-page");
+    $("li").removeClass("selected-link");
+    $(this).addClass("selected-link");
     $("main").fadeOut(pageDuration).css("display", "none");
     $("#resume").fadeIn(pageDuration);
 
@@ -153,8 +166,8 @@ $(function() {
     $("nav ul").removeClass("show-nav-mobile");
   });
   $("#contact-link").click(function() {
-    $("li").removeClass("selected-page");
-    $(this).addClass("selected-page");
+    $("li").removeClass("selected-link");
+    $(this).addClass("selected-link");
     $("main").fadeOut(pageDuration).css("display", "none");
     $("#contact").fadeIn(pageDuration);
 
@@ -171,7 +184,7 @@ function changeImg() {
   imageIndex = (imageIndex + 1) % (projectData.length);
   var imgText = projectData[imageIndex]["name"] + " (" + projectData[imageIndex]["year"]  + ")";
 
-  $(".slide-img > img")
+  $("#slide-img > img")
     .attr('src', "src/img/" + projectData[imageIndex]["imgFile"])
     .attr('alt', imgText);
   $(".img-caption").text(imgText);
